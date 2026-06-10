a995a54a91b0887a1be6d70272abb095297ff146
aaravgupta14 <213745276+aaravgupta14@users.noreply.github.com>
2026-06-10 22:11:17 +0530

    Update hardware_integration.ino

--- hardware_integration.ino
+++ hardware_integration.ino
@@ -1,3 +1,8 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 String msg="";
 
 void clearLEDs() {
@@ -5,6 +10,16 @@ void clearLEDs() {
   digitalWrite(3,LOW);
   digitalWrite(4,LOW);
   digitalWrite(5,LOW);
+}
+
+void showWelcome(String name) {
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("WELCOME");
+
+  lcd.setCursor(0,1);
+  lcd.print(name);
 }
 
 void setup() {
@@ -17,6 +32,15 @@ void setup() {
   pinMode(5,OUTPUT);
 
   clearLEDs();
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0,0);
+  lcd.print("VisionGuard");
+
+  lcd.setCursor(0,1);
+  lcd.print("Ready");
 }
 
 void loop() {
@@ -27,16 +51,39 @@ void loop() {
     msg.trim();
 
     clearLEDs();
-  if(msg=="AARAV")
-    digitalWrite(2,HIGH);
-  else if(msg=="ANSHU")
-    digitalWrite(3,HIGH);
-  else if(msg=="RISHU")
-    digitalWrite(4,HIGH);
-  else if(msg=="NAVYA")
-    digitalWrite(5,HIGH);
-  else if(msg=="UNKNOWN") {
+
+    if(msg=="AARAV") {
+      digitalWrite(2,HIGH);
+      showWelcome("AARAV");
+    }
+
+    else if(msg=="ANSHU") {
+      digitalWrite(3,HIGH);
+      showWelcome("ANSHU");
+    }
+
+    else if(msg=="RISHU") {
+      digitalWrite(4,HIGH);
+      showWelcome("RISHU");
+    }
+
+    else if(msg=="NAVYA") {
+      digitalWrite(5,HIGH);
+      showWelcome("NAVYA");
+    }
+
+    else if(msg=="UNKNOWN") {
+
+      lcd.clear();
+
+      lcd.setCursor(0,0);
+      lcd.print("UNKNOWN");
+
+      lcd.setCursor(0,1);
+      lcd.print("IMAGE SAVED");
+
       for(int i=0;i<3;i++) {
+
         digitalWrite(2,HIGH);
         digitalWrite(3,HIGH);
         digitalWrite(4,HIGH);
